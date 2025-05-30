08d245900d2165dc818818878cb0887bd091f8f2
zijie28 <zijieh@nycstudents.net>
2025-05-30 09:07:54 -0400

    asdasd

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import requests
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    response = requests.get("https://foodish-api.com/api")
+    cat = ["biryani", "butter-chicken", "dosa", "pasta", "rice", "burger", "dessert", "idly", "pizza", "samosa"]
+    b = random.choice(cat)
+    data = response.json()
+    e=data['image']
+    return render_template("index.html", picture=e, cata=b, cat=cat)
+@app.route("/<catagories>")
+def spec_cata(catagories):
+    response = requests.get(f"https://foodish-api.com/api/images/{catagories}/")
+    prop= response.json()
+    pic=prop.get("image")
+    try:
+        if prop.get('error') == 'Not found.':
+            return render_template("error.html")
+        else:
+            return render_template("spec.html", spec_pict=pic, food_type=catagories)
+    except KeyError:
+            return render_template("spec.html", spec_pict=pic, food_type=catagories)
+
+if __name__ == '__main__':
+    app.run(debug=True)
